fef04e91a57875fe34e93895f4ad75ebead1aa30
Jayakrishna Menon <jkrshnmenon@gmail.com>
2021-10-18 16:50:34 -0700

    updating stuff

--- bifrost/__init__.py
+++ bifrost/__init__.py
@@ -4,4 +4,4 @@ from .reducibility_detector import ReducibilityDetector
 # 1) File parser (DOT to Networkx)  <------------ Done
 # 2) File parser (GCC IR to Networkx)
 # 3) File parser (Clang IR to Networkx)
-# 4) Irreducibility detector
+# 4) Irreducibility detector  <------------ Done
