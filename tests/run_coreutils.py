52d30349f38afee29c7ba6c15e07416cc9cea80f
Jayakrishna Menon <jkrshnmenon@gmail.com>
2021-10-19 16:39:31 -0700

    coreutils is now runnable

---
+++ tests/run_coreutils.py
@@ -0,0 +1,49 @@
+import os
+import sys
+import pdb
+import json
+import logging
+
+sys.path.append(os.path.abspath(os.path.join(__file__, '../..')))
+
+from bifrost import DotParser, GCC_Utils, ReducibilityDetector
+
+def setup_loggers(name):
+    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+
+
+def main(command):
+    irreducible = []
+    utils = GCC_Utils(pre_args=cmd['pre_args'])
+    for dot_file in utils.generate_dot_files(cmd['filename']):
+        parser = DotParser(dot_file)
+        for graph in parser.graph:
+            roots = parser.get_roots(graph)
+            if len(roots) < 1:
+                print(f"Cannot find roots for {dot_file}")
+                continue
+            checker = ReducibilityDetector(graph)
+            if checker.is_reducible(roots[0]) is False:
+                print(f"Irreducible graph in {dot_file}")
+                irreducible.append(f"{dot_file}")
+    print(irreducible)
+
+
+if __name__ == '__main__':
+
+    setup_loggers('bifrost.gcc_utils')
+    setup_loggers('bifrost.dot_parser')
+    setup_loggers('bifrost.reducibility_detector')
+    with open("tests/coreutils_commands.json", "r") as f:
+        commands = json.load(f)
+    
+    for cmd in commands:
+        try:
+            main(cmd)
+        except:
+            pdb.set_trace()
